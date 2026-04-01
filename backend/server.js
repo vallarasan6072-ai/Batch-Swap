4b3dcdf42e2275515cdb3cfcfb1a41f60e66d7ec
vallarasan <vallarasan6072@gmail.com>
2026-04-01 14:28:25 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -10,4 +10,4 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth.js"));
 app.use("/api", require("./routes/swap.js"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(8080, () => console.log("Server running on port 8080"));
